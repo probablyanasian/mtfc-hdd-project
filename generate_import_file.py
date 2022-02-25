3f588d9c95b4ec6eeb7e3acc00c52e3efa4c606e
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2022-02-24 21:26:41 -0800

    fix generate_import_file.py

--- generate_import_file.py
+++ generate_import_file.py
@@ -9,4 +9,4 @@ with open(f'./imports/custom_import_{file_time}.sql', 'w+') as outfile:
 	for file in files:
 		outfile.write(f'.import {file} drive_stats_{file_time}\n')
 
-	outfile.write('.echo off\n.mode list\nDELETE FROM drive_stats WHERE model = \'model\';')
+	outfile.write(f'.echo off\n.mode list\nDELETE FROM drive_stats_{file_time} WHERE model = \'model\';')
